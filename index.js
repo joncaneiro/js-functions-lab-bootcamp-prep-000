c1cfabefa592e315dc6a3abce791f63bbab39bcb
Jon Caneiro <joncaneiro@gmail.com>
2019-07-23 21:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+function happyHolidays () {
+  return "Happy Holidays!"
+}
+
+function HappyHolidaysTo(person) {
+  console.log("Happy Holidays, ${person}!")
+}
+
+function happyHolidayTo(holiday, person) {
+  console.log("Happy ${holiday}, ${person}!")
+}
+
+function holidayCountdown(holiday, days) {
+  console.log("It's ${days} days until ${holiday}")
+}
+
